3fbfad8fcff6ac25c77a77bc8618e39fbacea478
Aamir-Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2022-12-04 23:29:36 +0530

    Create BasicCalculator.py

---
+++ BasicCalculator.py
@@ -0,0 +1,14 @@
+n = int(input("Enter your number 1"))
+m = int(input("Enter your number 2"))
+ans1 = n + m
+print("Addition of two number ",ans1 )
+ans2 = n - m
+print("Subtraction of two number ",ans2 )
+ans3 = n * m
+print("Product of two number ",ans3 )
+ans4 = n / m
+print("Divition of two number ",ans4 )
+ans5 = n // m
+print("Floor divition of two number ",ans5 )
+ans6 = n % m
+print("Remainder of two number ",ans6 )
