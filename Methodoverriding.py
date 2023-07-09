d831770977c69523de9df1caa547c8a541e94bd5
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2023-07-10 01:27:44 +0530

    Add files via upload

---
+++ Methodoverriding.py
@@ -0,0 +1,17 @@
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x * self.y
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        super().__init__(radius, radius)
+    def area(self):
+        return 3.14 * super().area()
+
+a1 = Circle(3)
+print(a1.area())
